1c4caa5553aaffb9a5c38813b6d2071e30281c6b
Saravanan Palanivel <16950785+saravananpalanivel@users.noreply.github.com>
2018-02-07 18:01:44 +0530

    Template questions removed

--- CSharp_Quiz/Questions/Q_List.cs
+++ CSharp_Quiz/Questions/Q_List.cs
@@ -3,43 +3,7 @@
     class Q_List
     {
 
-        /// <summary>
-        /// Write a program and continuously ask the user to enter different names, until the user presses Enter 
-        /// (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. 
-        /// Display the reversed name on the console.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display 
-        /// an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them 
-        /// and display the result on the console.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may 
-        /// include duplicates. Display the unique numbers that the user has entered.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is 
-        /// empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display 
-        /// the 3 smallest numbers in the list.
-        /// 
-        /// </summary>
-        /// 
-
+        // TBA
 
     }
 }
--- CSharp_Quiz/Questions/Q_Loops.cs
+++ CSharp_Quiz/Questions/Q_Loops.cs
@@ -2,41 +2,8 @@
 {
     class Q_Loops
     {
-        /// <summary>
-        /// Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. 
-        /// Display the result on the console.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program and continuously ask the user to enter a number. The loop terminates when the user 
-        /// enters “ok". Calculate the sum of all the previously entered numbers and display it on the console.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program which takes a single argument from the console, computes the factorial and prints the 
-        /// value on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
-        /// and display it as 5! = 120.
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. 
-        /// If the user guesses the number, display “You won". Otherwise, display “You lost".
-        /// </summary>
-        /// 
-
-
-        /// <summary>
-        /// Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the 
-        /// numbers and display it on the result. For example, if the user enters “5, 3, 8, 1, 4", the program should 
-        /// display 8 on the console.
-        /// </summary>
-        /// 
+        
+        // TBA
 
     }
 }
