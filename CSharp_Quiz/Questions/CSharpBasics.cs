e3020390b29c8208f26556c92aceca6f46e0357a
Saravanan Palanivel <16950785+saravananpalanivel@users.noreply.github.com>
2018-02-13 11:05:58 +0530

    Questions added

--- CSharp_Quiz/Questions/CSharpBasics.cs
+++ CSharp_Quiz/Questions/CSharpBasics.cs
@@ -2,8 +2,42 @@
 {
     class CSharpBasics
     {
-        
-        // TBA
+
+        /// <summary>
+        /// Write a program and ask the user to enter the width and height of an image. Then tell if the image 
+        /// is landscape or portrait.
+        /// </summary>
+        /// 
+
+
+
+
+        /// <summary>
+        /// Write a program which takes a single argument from the console, computes the factorial and prints the 
+        /// value on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
+        /// and display it as 5! = 120.
+        /// </summary>
+        /// 
+
+
+
+
+        /// <summary>
+        /// Ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. 
+        /// Display the reversed name on the console.
+        /// </summary>
+        /// 
+
+
+
+        /// <summary>
+        /// Write a program and ask the user to enter a few numbers separated by a hyphen. Work out 
+        /// if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", 
+        /// display a message: "Consecutive"; otherwise, display "Not Consecutive".
+        /// </summary>
+        /// 
+
+
 
     }
 }
